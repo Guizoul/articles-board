15a6821d50b3137ee419512ddb5ee1af67ada2ee
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-08-05 20:58:41 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,35 +8,20 @@ class App extends Component {
     super();
 
     this.state = {
-      name: { firstName: "Abdellah", lastName: "GUIZOUL" },
-      company: "AFD",
+      monsters: [],
     };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi I'm {this.state.name.firstName} I work at {this.state.company} .
-          </p>
-          <button
-            onClick={() => {
-              this.setState({ name: { firstName: "Andrie" } });
-            }}
-          >
-            Change Name
-          </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
