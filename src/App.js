5508f9c2af8d3997a644a38a8d3d25baf6e2fd04
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-08-05 20:59:19 +0100

    calling api

--- src/App.js
+++ src/App.js
@@ -12,6 +12,21 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
   render() {
     return (
       <div className="App">
