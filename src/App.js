ad537a32bd38387151956ef49d774c28f010b911
Guizoul <guizoul.abdellah@ine.inpt.ma>
2022-08-10 22:55:44 +0100

    initial search

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log(1);
   }
 
   componentDidMount() {
+    console.log(3);
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
       .then((users) =>
@@ -28,8 +30,30 @@ class App extends Component {
   }
 
   render() {
+    console.log(2);
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            // event.target.value
+            this.setState(
+              () => {
+                return {
+                  monsters: this.state.monsters.filter((monster) =>
+                    monster.startsWith(event.target.value)
+                  ),
+                };
+                return { monsters: users };
+              },
+              () => {
+                console.log(this.state);
+              }
+            );
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
